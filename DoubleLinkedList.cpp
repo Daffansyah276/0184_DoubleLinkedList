e8fac18442295e7cf1451f07cd9cf8fa8ca05960
Daffansyah276 <daffansyaharya@gmail.com>
2025-05-15 11:30:12 +0700

    menambahkan fungsi search

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -176,7 +176,41 @@ public:
     
     }
 
- 
+    void search()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty " << endl;
+            return;
+        }
+
+        int rollNo;
+        cout << "\nEnter the roll number of the student to be searched: ";
+        cin >> rollNo;
+
+        Node *current = START;
+
+        //step 1: Traverse to find matching roll number 
+        while (current != NULL && current->noMhs != rollNo)
+            current = current->next;
+
+        //step 2:Output result 
+        if (current == NULL)
+        {
+            cout << "Record not found " << endl;
+        }
+        else
+        {
+            cout << "Record not found\n ";
+            cout << "Roll Number : " << current->noMhs << endl;
+
+        }
+    }
+
+};
+
+
+
         
     
     
