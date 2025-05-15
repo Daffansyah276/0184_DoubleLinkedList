2c1400e8a497181aee6de011acdbb6ce742fc30c
Daffansyah276 <daffansyaharya@gmail.com>
2025-05-15 10:09:07 +0700

    menambahkan langkah-langkah

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -29,12 +29,34 @@ public:
         cout << "/nEnter the roll number of the student: ";
         cin >> nim;
 
-      
+        //step 1: Allocate memory for new node 
+        Node *newNode = new Node();
 
+        //step 2: Assign vaalue to the data fields
+        newNode->noMhs = nim;
 
+        //step3 : insert at begining if list is empty or nim is smallest
+        if (START == NULL || nim <= START->noMhs) 
+        {
+            if (START != NULL && nim == START->noMhs )
+            {
+                cout << "\nDuplicate number not allowed " << endl;
+                return;
+            }
 
+            //step 4: newNode.next = start
+            newNode->next = START;
 
+            //step5 : START.prev = newNode (if START exits)
+            if (START != NULL)
+                START->prev = newNode;
 
+            //step 6: newnode.prev = null
+            newNode->prev = NULL;
+
+            //step 7 : start = newnode
+            START = newNode;
+            return;
 
         }
 
